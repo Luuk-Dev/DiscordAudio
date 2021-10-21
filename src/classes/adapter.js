2d4b54d4f795839a7dc4fb54f77c7815364440df
Luuk <70959239+Luuk-Dev@users.noreply.github.com>
2021-10-21 20:55:23 +0200

    Create adapter.js

---
+++ src/classes/adapter.js
@@ -0,0 +1,21 @@
+const {createAdapter} = require('../adapter.js');
+const constants = require('../util/constants.js');
+
+class Adapter{
+    /**
+     * Creates an adapter for voice connections. Works for multiple Discord.js versions.
+     * @param {object} channel The voice channel to create an adapter for.
+     * @returns A Discord.js music adapter.
+     * @example
+     * const adapter = new Adapter(<channel>);
+     */
+    constructor(channel){
+        if(typeof channel === 'undefined' || channel === undefined || channel === "") throw new Error(constants.ERRORMESSAGES.REQUIRED_PARAMETER_CHANNEL);
+        
+        const adapter = createAdapter(channel);
+
+        return adapter;
+    }
+}
+
+module.exports = {Adapter};
