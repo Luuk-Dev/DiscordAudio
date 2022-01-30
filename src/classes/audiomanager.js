f1cd3188c6e4f55b626f77c97620409a600a7faf
Ari Meisels <arimeisels3@gmail.com>
2022-01-30 14:40:18 -0500

    Stopping loop was impossible
    The code for turning a loop off in the AudioManager class was 0. The code then would check `if(!channel || !loop)` which would return true because loop is 0. Error.

--- src/classes/audiomanager.js
+++ src/classes/audiomanager.js
@@ -103,14 +103,14 @@ class AudioManager extends EventEmitter{
   loop(channel, loop){
     if(!channel || !loop) throw new Error(constants.ERRORMESSAGES.REQUIRED_PARAMETERS_LOOP);
     if(isNaN(loop)) throw new Error(constants.ERRORMESSAGES.LOOP_PARAMETER_NAN);
-    if(loop < 0 || loop > 2) throw new Error(constants.ERRORMESSAGES.LOOP_PARAMETER_INVALID);
+    if(loop < 1 || loop > 3) throw new Error(constants.ERRORMESSAGES.LOOP_PARAMETER_INVALID);
     if(!globals[channel.id]) throw new Error(constants.ERRORMESSAGES.PLAY_FUNCTION_NOT_CALLED);
     globals[channel.id].set(`loop`, loop);
   };
   looptypes = {
-    off: 0,
-    loop: 1,
-    queueloop: 2
+    off: 1,
+    loop: 2,
+    queueloop: 3
   };
   stop(channel){
     if(!channel) throw new Error(constants.ERRORMESSAGES.REQUIRED_PARAMETER_CHANNEL);
