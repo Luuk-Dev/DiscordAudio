e52e7f0303f030e14355cde3a5198cf1ecd63d1d
Luuk <70959239+Luuk-Dev@users.noreply.github.com>
2023-10-21 16:29:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-/**
- * @exports Player Creates a music player.
- * @exports Connection Creates a connection to play stream.
- * @exports Broadcast Creates a broadcast to play a stream in multiple guilds.
- * @exports Adapter Creates an adapter. Works for multiple Discord.js versions.
- * @exports AudioManager Creates an audio manager.
- */
- const constants = require('./src/util/constants.js');
- const audversion = constants.version.split(".");
- if(Number(audversion[0]) < 2) console.warn(constants.WARNMESSAGES.VERSION_WARNING);
- 
- module.exports = {
+module.exports = {
     Player: require('./src/classes/player').Player,
     Broadcast: require('./src/classes/broadcast').Broadcast,
     Connection: require('./src/classes/connection').Connection,
