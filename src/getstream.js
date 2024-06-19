8c4685eac2abb4f9929710e2a1fbdd5440b4677d
Luuk <70959239+Luuk-Dev@users.noreply.github.com>
2024-06-19 16:40:34 +0200

    Update getstream.js

--- src/getstream.js
+++ src/getstream.js
@@ -1,29 +1,58 @@
-const { spawn } = require('child_process');
+const { Readable } = require('stream');
 const prism = require('prism-media');
 
 const playAudio = (url, standardFilters = [], customFilters = [], ffmpeg) => {
-    if(!ffmpeg) return url;
+    if(!ffmpeg){
+        if(url instanceof Readable){
+            const opus = new prism.opus.Encoder({ rate: 48000, channels: 2, frameSize: 960 });
 
-    const iFilter = standardFilters.filter(f => f === "--audio-url")[0];
-    if(!iFilter) throw new Error('Invalid filters');
-    const indexFilter = standardFilters.indexOf(iFilter);
-    standardFilters[indexFilter] = url;
+            opus.on('close', () => {
+                opus.destroy();
+            });
+
+            url.on('error', err => opus.emit('error', err));
+
+            return url.pipe(opus);
+        } else return url;
+    }
+
+    if(typeof url === 'string'){
+        const iFilter = standardFilters.filter(f => f === "--audio-url")[0];
+        if(!iFilter) throw new Error('Invalid filters');
+        const indexFilter = standardFilters.indexOf(iFilter);
+        standardFilters[indexFilter] = url;
+    } else {
+        const indexInput = standardFilters.indexOf('--audio-url');
+        if(indexInput >= 0) standardFilters[indexInput] = "-";
+    }
 
     let custom = ['-af', customFilters.join(',')];
 
-    let ls = spawn('ffmpeg', [...standardFilters, ...(customFilters.length > 0 ? custom : []), 'pipe:1']);
+    let ffmpegArgs = [...standardFilters, ...(customFilters.length > 0 ? custom : [])];
+    if(typeof url === 'string') ffmpegArgs.push('pipe:1');
+    let ls = new prism.FFmpeg({
+        args: [...standardFilters, ...(customFilters.length > 0 ? custom : [])]
+    });
+
+    let output = ls;
+
+    if(url instanceof Readable){
+        output = url.pipe(ls);
+    }
     
     const opus = new prism.opus.Encoder({ rate: 48000, channels: 2, frameSize: 960 });
+    
+    const outputPipe = output.pipe(opus);
+
+    output.on('error', err => outputPipe.emit('error', err));
 
     opus.on('close', () => {
-        ls.kill('SIGKILL');
+        ls.destroy();
         ls = null;
         opus.destroy();
     });
-    
-    ls.stdout.pipe(opus);
 
-    return opus;
+    return outputPipe;
 }
 
 module.exports = { playAudio };
